633dd0cb97491bb9c86f2f4325cfe8c6cd31090c
james7mac <32667561+james7mac@users.noreply.github.com>
2020-05-09 13:57:22 +1000

    refactored payslips and payslip class

--- slip.py
+++ slip.py
@@ -1,7 +1,7 @@
 #! python3
 # slip.py - retrieves pdf from adp and then scrapes data from that pdf
 
-import time, pyautogui, os, tabula, csv, openpyxl, glob, pickle, logging
+import time, pyautogui, os, tabula, csv, openpyxl, glob, pickle, logging, shutil
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
@@ -13,18 +13,71 @@ logging.debug('Start of program')
 
 if os.environ['COMPUTERNAME'] == 'LAPTOP':
     working_dir =   r'C:\Users\james\PycharmProjects\payroll_and_roster'
-    slip_folder =       r'C:\Users\james\PycharmProjects\payroll_and_roster'
+    slip_inbox =  ''
+    gecko_driver = r"C:\Users\james\Google Drive\System Files\geckodriver.exe"
 
 elif os.environ['COMPUTERNAME'] == 'JAMESPC':
     working_dir = r'A:\Python\payroll_and_roster'
-    download_folder = ''
+    gecko_driver = r'A:\Program Files\geckodriver.exe'
+    slip_inbox = r'C:\Users\james\Downloads'
 else:
     Exception()
 
 
-class payslip:
+class Payslips:
+    def __init__(self, password):
+        self.url = r'https://secure.adppayroll.com.au/'
+        self.work_ID = 'PR00002'
+        self.employee_ID = '500435'
+        self.password = password
+        self.slips = []
+
+    def get_payslip(self):
+        browser = webdriver.Firefox(executable_path=gecko_driver)
+        browser.get(self.url)
+        workID_elem = browser.find_element_by_id('client_id')
+        employeeID_elem = browser.find_element_by_id('user_id')
+        password_elem = browser.find_element_by_id('password')
+        employeeID_elem.send_keys(self.employee_ID)
+        workID_elem.send_keys(self.work_ID)
+        password_elem.send_keys(self.password)
+        password_elem.send_keys(Keys.ENTER)
+        time.sleep(4)
+        browser.get(r'https://my.adppayroll.com.au/webapp/paydetails/payslips')
+        time.sleep(8)
+        # put '1' between both halves to reviece most recent payslip.
+        # put '2' to recieve second newest
+        xpath_p1 = 'html[1]/body[1]/app-root[1]/div[1]/app-layout-component[1]/app-layout[1]/div[1]/div[2]/div[2]/div[2]/app-view-payslips[1]/div[1]/tile-content[1]/app-payslips-download[1]/adp-table[1]/table[1]/tbody[1]/tr['
+        xpath_p2 = ']/td[3]/ng-component[1]/adp-button[1]'
+        browser.find_element_by_xpath(xpath_p1 + '1' + xpath_p2).click()
+        time.sleep(3)
+        pyautogui.press('enter')
+        time.sleep(2)
+        browser.close()
+        time.sleep(2)
+        slip_file = most_recent_file(slip_inbox, 'pdf')
+        new_slip_path = working_dir + '\\' + 'payslips'
+        shutil.move(slip_file, new_slip_path)
+        return new_slip_path
+
+    def process_payslip(self, slip_path):
+        payslip = Payslip(slip_path)
+        payslip.scan_pdf()
+        payslip.extract_dates()
+        payslip.compile_advice()
+        payslip.compile_decuctions()
+        payslip.compile_leave()
+        payslip.get_hours_earned()
+        payslip.get_hours_worked()
+        payslip.rename_slip()
+        payslip.remove_garbage()
+        self.slips.append(payslip)
+
+
+
+class Payslip:
     def __init__(self, path):
-        self.pdf_path = path = "\\" + 'payslips'
+        self.pdf_path = path
         self.scan_pdf()
         self.advice_table = self.get_advice_table()
         self.standard_table = self.get_standard_table()
@@ -32,15 +85,15 @@ class payslip:
     def scan_pdf(self):
         csv_filename = os.path.dirname(self.pdf_path) + '\\' + 'tempfile.csv'
         print(csv_filename)
-        tabula.convert_into(os.path.basename(self.pdf_path), csv_filename, output_format="csv", pages='all')
-        file = open("tempfile.csv")
+        tabula.convert_into(self.pdf_path, csv_filename, output_format="csv", pages='all')
+        file = open(csv_filename)
         tabReader = csv.reader(file)
         tablist = list(tabReader)
         self.raw_table =  tablist
 
     def extract_dates(self):
-        fornight_start = self.table[2][0]
-        fortnight_end = self.table[2][1]
+        fornight_start = self.standard_table[2][0]
+        fortnight_end = self.standard_table[2][1]
         format = '%d/%m/%Y'
         return datetime.strptime(fornight_start, format), datetime.strptime(fortnight_end, format)
 
@@ -99,6 +152,14 @@ class payslip:
 
     def get_hours_earned(self):
         pass
+
+    def rename_slip(self):
+        pass
+
+    def remove_garbage(self):
+        pass
+
+
 
 
 
@@ -216,44 +277,12 @@ class RosterDay:
         return data
 
 
-class Payslips:
-    def __init__(self, password):
-        self.url = r'https://secure.adppayroll.com.au/'
-        self.work_ID = 'PR00002'
-        self.employee_ID = '500435'
-        self.password = password
-        now = time.time() - 60*60*24*14
-
-
-
-    def get_payslip(self):
-        browser = webdriver.Firefox(executable_path=r"C:\Users\james\Google Drive\System Files\geckodriver.exe")
-        browser.get(self.url)
-        workID_elem = browser.find_element_by_id('client_id')
-        employeeID_elem = browser.find_element_by_id('user_id')
-        password_elem = browser.find_element_by_id('password')
-        employeeID_elem.send_keys(self.employee_ID)
-        workID_elem.send_keys(self.work_ID)
-        password_elem.send_keys(self.password)
-        password_elem.send_keys(Keys.ENTER)
-        time.sleep(4)
-        browser.get(r'https://my.adppayroll.com.au/webapp/paydetails/payslips')
-        time.sleep(8)
-        #put '1' between both halves to reviece most recent payslip.
-        #put '2' to recieve second newest
-        xpath_p1 = 'html[1]/body[1]/app-root[1]/div[1]/app-layout-component[1]/app-layout[1]/div[1]/div[2]/div[2]/div[2]/app-view-payslips[1]/div[1]/tile-content[1]/app-payslips-download[1]/adp-table[1]/table[1]/tbody[1]/tr['
-        xpath_p2 = ']/td[3]/ng-component[1]/adp-button[1]'
-        browser.find_element_by_xpath(xpath_p1 + '1' + xpath_p2).click()
-        time.sleep(3)
-        pyautogui.press('enter')
-        time.sleep(2)
-        browser.close()
-
 
 def most_recent_file(location, extention):
     #returns the most recent file with extention
-    path = location + '/' + extention
+    path = location + '\\' + extention
     list_of_files = glob.glob(path)  # * means all if need specific format then *.csv
+    print(list_of_files)
     latest_file = max(list_of_files, key=os.path.getctime)
     return latest_file
 
@@ -263,7 +292,6 @@ def most_recent_file(location, extention):
 def most_recent_file(location, extention):
     #returns the most recent file with extention
     path = location + '\\*.' + extention
-    print(path)
     list_of_files = glob.glob(path)  # * means all if need specific format then *.csv
     latest_file = max(list_of_files, key=os.path.getctime)
     return latest_file
@@ -275,6 +303,11 @@ if __name__ == '__main__':
     else:
         roster = Roster(working_dir)
     print(roster.compiled_roster[0].shifts)
+    payslips = Payslips('t8C3&Lq9uTy0')
+    slip = most_recent_file(working_dir + '\\payslips', 'pdf')
+    print(slip)
+    payslips.process_payslip(slip)
+
     with open(working_dir+'\\'+'roster.pickle', 'wb') as file:
             pickle.dump(roster, file)
 
